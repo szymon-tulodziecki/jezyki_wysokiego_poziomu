0b8c7ba50421af8a6f9e58d05da0c53aa83e7b43
szymon-tulodziecki <21312@student.ans-elblag.pl>
2025-10-06 11:22:46 +0200

    Dodanie trzeciego (częśc druga) pliku zadania

--- lab_1/zad_3.js
+++ lab_1/zad_3.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 const hostname = '127.0.0.1';
 const port = 8888;
 const server = http.createServer(function(request, response) {
@@ -7,5 +7,25 @@ response.setHeader('Content-Type', 'text/plain');
 response.end('Witaj, z serwera node.js !\n');
 });
 server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+*/
+
+const http = require('http');
+const path = require("path");
+const util = require("util");
+const v8 = require("v8");
+const hostname = '127.0.0.1';
+const port = 8888;
+const server = http.createServer((request, response) => {
+response.statusCode = 200;
+response.setHeader('Content-Type', 'text/plain');
+response.end('Hello, from node.js serwer !\n');
+});
+server.listen(port, hostname, () => {
+util.log(v8.getHeapStatistics());
+console.log(path.basename(__filename));
+util.log(path.join(__dirname, 'uploads','images'));
 console.log(`Server running at http://${hostname}:${port}/`);
 });
