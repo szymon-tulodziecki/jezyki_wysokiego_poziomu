2905f832213345951197b9538889382196628bc9
szymon-tulodziecki <21312@student.ans-elblag.pl>
2025-10-06 11:19:38 +0200

    Dodanie trzeciego pliku zadania

---
+++ lab_1/zad_3.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 8888;
+const server = http.createServer(function(request, response) {
+response.statusCode = 200;
+response.setHeader('Content-Type', 'text/plain');
+response.end('Witaj, z serwera node.js !\n');
+});
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
