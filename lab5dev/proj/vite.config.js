0e4cdff5b8c553b20d585f2b9b56a366e1f3e0b0
Szymon Tułodziecki <21312@student.ans-elblag.pl>
2025-11-03 11:01:37 +0100

    fix: correct HTML structure and update asset paths in index.html files

--- lab5dev/proj/vite.config.js
+++ lab5dev/proj/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jezyki_wysokiego_poziomu/lab5prod/',
   plugins: [react()],
+  build: {
+    outDir: '../../lab5prod',
+    emptyOutDir: true,
+  },
 })
